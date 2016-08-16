fdbb87352fa43331b6b5ad3cfe144a14af92e508
Dustin Reed <dmreedy1@gmail.com>
2016-08-15 19:04:20 -0500

    fixed html input pattern on submit

--- strings.js
+++ strings.js
@@ -1,4 +1,6 @@
 let results = document.getElementById("results");
+let testBtn = document.getElementById("testbtn");
+let testInput = document.getElementById("testinput");
 
 function reversal(inputStr) {
     return inputStr.split("").reverse().join("");
@@ -9,29 +11,15 @@ function alphabits(inputStr) {
 }
 
 function palindrome(inputStr) {
-    return (inputStr === reversal(inputStr)) ? "Your string is a palidrome" : "";
+    return (inputStr === reversal(inputStr)) ? "Your string is a palidrome!" : "";
 }
 
 var testString = "radar";
 
 function outputValues() {
+    testString = testInput.value;
     results.innerHTML = "";
     results.innerHTML += "<div>" + reversal(testString) + "</div>"
     results.innerHTML += "<div>" + alphabits(testString) + "</div>"
     results.innerHTML += "<div>" + palindrome(testString) + "</div>"
 }
-
-let testBtn = document.getElementById("testbtn");
-let testInput = document.getElementById("testInput");
-
-testBtn.addEventListener("click", function() {
-    testString = testInput.value;
-    outputValues();
-}, false);
-
-testInput.addEventListener("keydown", function() {
-    if (window.event.keyCode == '13') {
-        testString = testInput.value;
-        outputValues();
-    }
-}, false);
